2a2d2f7f9a9aa27f37bee82768f8acbfd42b4f96
Joe Netti <joe@netti.dev>
2022-12-30 15:49:30 -0700

    very basic website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let poems = [
+        {
+            name: "A Boy Sits Upon A Tower",
+            route: "/a-boy-sits-upon-a-tower"
+        },
+        {
+            name: "A Door For Change",
+            route: "/a-door-for-change"
+        },
+        {
+            name: "Peach Picking",
+            route: "/peach-picking"
+        },
+    ]
+</script>
+
+<h1>Poetry</h1>
+
+{#each poems as {name, route}, i}
+<h3>
+    <a href={route}>{name}</a>
+</h3>
+{/each}
+
+<style>
+    h1 {
+        padding-bottom: .3em;
+        font-size: 2em;
+        border-bottom: 1px solid #eee;
+        border-bottom-color: rgb(238, 238, 238);
+    }
+</style>
---
+++ src/routes/peach-picking/+page.svelte
@@ -0,0 +1,42 @@
+<body>
+    Peach Picking
+    <br> 
+    <br> We wrap ourselves
+    <br> In light linen coats
+    <br> Grey woolen socks
+    <br> And leather shoes
+    <br> To journey
+    <br> With baskets in hand
+    <br> To narrow streets
+    <br> Of yellow peach trees
+    <br> That meander through the wood
+    <br> 
+    <br> We follow the trail up a hill
+    <br> To feel the sky widen
+    <br> Before us
+    <br> To dance freely
+    <br> As it does
+    <br> To pause.
+    <br> To stitch together
+    <br> Shapes, lines, colors 
+    <br> Where trees become forest
+    <br> Leaves--a canopy
+    <br> 
+    <br> Dancing swiftly, swiftly
+    <br> Down blades of grass 
+    <br> Through matchbox meadows
+    <br> Patchwork paths
+    <br> We are free.
+    <br> Weaving in and out 
+    <br> Of expansive fabrics
+    <br> Crimson bows
+    <br> Against the horizon
+    <br> Tapping toes
+    <br> On the rising sun   
+</body>
+
+<style>
+    body {
+        text-align: center;
+    }
+</style>
