531ce023e5f4fa2bf66ec18a8eca9c7c298fe6f7
Joe Netti <joe@netti.dev>
2023-01-02 20:39:03 -0700

    add nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+			base: process.env.NODE_ENV === "production" ? "/nettijoe96.github.io" : "",
 		},
 	}
 };
