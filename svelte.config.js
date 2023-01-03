8072dcf9c10996d8b79eb11e05c009a600ee4fcb
Joe Netti <joe@netti.dev>
2023-01-02 20:43:41 -0700

    changing base path

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/nettijoe96.github.io" : "",
+			base: process.env.NODE_ENV === "production" ? "https://nettijoe96.github.io/" : "",
 		},
 	}
 };
